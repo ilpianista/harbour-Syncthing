001287e1bb0c14e4639472730eecb1c6e57e3d8b
Nathan <nthn@unseen.is>
2026-08-23 14:57:35 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Harbour Syncthing/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-syncthing/translations/nl/

--- translations/harbour-syncthing-nl.ts
+++ translations/harbour-syncthing-nl.ts
@@ -5,26 +5,26 @@
     <name>FolderDelegate</name>
     <message>
         <source>Last scan: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatste scan: %1</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Openen in browser</translation>
     </message>
     <message>
         <source>Refresh folders list</source>
-        <translation type="unfinished"></translation>
+        <translation>Lijst met mappen vernieuwen</translation>
     </message>
     <message>
         <source>No folders</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen mappen</translation>
     </message>
     <message>
         <source>Pull down to refresh list</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek omlaag om de lijst te vernieuwen</translation>
     </message>
 </context>
 </TS>
