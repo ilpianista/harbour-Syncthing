f5a47eb59a85fd4611b059fed75b0d22e6919810
Nathan <nthn@unseen.is>
2026-08-23 14:56:32 +0200

    Added translation using Weblate (Dutch)

---
+++ translations/harbour-syncthing-nl.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>FolderDelegate</name>
+    <message>
+        <source>Last scan: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Open in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh folders list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to refresh list</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
